80e05291785d727df938238ff4d697a05ac945b9
github_cloudlabsuser_1998 <github_cloudlabsuser_1998@cloudlabsaiuser.com>
2024-06-18 08:07:07 +0000

    Deploy ARM template for Azure App Service on Free plan

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,18 +11,32 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            // Find the user in the userlist by their ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            // If no user is found with the provided ID, return a HttpNotFound result
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            // If a user is found, pass the user object to the Details view
+            return View(user);
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            // Simply return the Create view. The view will contain the form for creating a new user.
+            return View();
         }
  
         // POST: User/Create
@@ -30,6 +44,18 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            // Check if the model state is valid
+            if (ModelState.IsValid)
+            {
+                // Add the new user to the userlist
+                userlist.Add(user);
+
+                // Redirect to the Index action to show the list of users
+                return RedirectToAction("Index");
+            }
+
+            // If the model state is not valid, return the same view to the user for correction
+            return View(user);
         }
  
         // GET: User/Edit/5
@@ -37,6 +63,17 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // Find the user in the userlist by their ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            // If no user is found with the provided ID, return a HttpNotFound result
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            // If a user is found, pass the user object to the Edit view
+            return View(user);
         }
  
         // POST: User/Edit/5
@@ -48,12 +85,46 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            // Find the user in the userlist by their ID
+            var userToUpdate = userlist.FirstOrDefault(u => u.Id == id);
+
+            // If no user is found with the provided ID, return a HttpNotFound result
+            if (userToUpdate == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Check if the model state is valid
+            if (ModelState.IsValid)
+            {
+                // Update the user's properties
+                userToUpdate.Name = user.Name;
+                userToUpdate.Email = user.Email;
+                // Add any other properties that need to be updated
+
+                // Redirect to the Index action to show the updated list of users
+                return RedirectToAction("Index");
+            }
+
+            // If the model state is not valid, return the Edit view to display any validation errors
+            return View(user);
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            // Find the user in the userlist by their ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            // If no user is found with the provided ID, return a HttpNotFound result
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            // If a user is found, pass the user object to the Delete view
+            return View(user);
         }
  
         // POST: User/Delete/5
@@ -61,6 +132,20 @@ namespace CRUD_application_2.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            // Find the user in the userlist by their ID
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            // If no user is found with the provided ID, return a HttpNotFound result
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Remove the user from the userlist
+            userlist.Remove(user);
+
+            // Redirect to the Index action to show the updated list of users
+            return RedirectToAction("Index");
         }
     }
 }
